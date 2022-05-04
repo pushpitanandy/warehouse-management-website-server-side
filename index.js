94eb4cae9f1e1f4395ca2ee3a1b7e86c60d4f830
pushpitanandy <nandypushpita@gmail.com>
2022-05-04 21:30:05 +0530

    data can be read from database

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zzmw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Inventory DB connected');
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const perfumeCollection = client.db('perfumeInventory').collection('perfume');
+
+        app.get('/perfume', async (req, res) => {
+            const query = {};
+            const cursor = perfumeCollection.find(query);
+            const perfumes = await cursor.toArray();
+            res.send(perfumes);
+        });
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running inventory server');
